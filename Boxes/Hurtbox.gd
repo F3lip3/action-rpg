db41a38c12fa996daa541ee29cae6f59b203346a
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2021-05-04 00:38:10 -0300

    adds player health ui

--- Boxes/Hurtbox.gd
+++ Boxes/Hurtbox.gd
@@ -12,7 +12,7 @@ signal invincibility_ended
 func _on_Timer_timeout():
 	self.invincible = false
 	
-func create_hit_effect(_area):
+func create_hit_effect():
 	var effect = HitEffect.instance()
 	var main = get_tree().current_scene
 	main.add_child(effect)
