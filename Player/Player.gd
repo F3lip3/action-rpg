5aa25cd71eb73abdaf58c61c445943ad5fa54d83
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2021-05-06 00:38:38 -0300

    adds enemy wander state

--- Player/Player.gd
+++ Player/Player.gd
@@ -23,6 +23,7 @@ onready var hurtbox = $Hurtbox
 onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
+	randomize()
 	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
 	swordHitbox.knockback_vector = roll_vector
